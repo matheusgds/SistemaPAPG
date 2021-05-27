ae9b1f8feaee29fb30b4d89b8254b5a68de9c107
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-05-27 18:36:03 -0300

    criado index vazio e css vazio
    criado index vazio e o css está vazio tambem, iniciando a codificação

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width,initial-scale=1.0"/>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+        <title>Sistema PAPG</title>
+    </head>
+    <body>
+        <div name="paginabotoes" id="paginabotoes">
+            <input class="inputa1" type="text" name="a1" placeholder="Termo A1">
+            
+        </div>
+        <?php
+            
+        ?>
+    </body>
+</html>
