6738ca5fa5389f1c2b5adb11f59d6e148376b69e
matheus giacomelli dos santos <matheusgds66@gmail.com>
2021-06-02 01:13:28 -0300

    tudo esta pronto menos a parte do grafico
    fiz toda parte de verificacao de dados e tambem a parte mostragem dos dados, agora so falta mais a parte do grafico

--- ArquivosPHP/CalculaValores.php
+++ ArquivosPHP/CalculaValores.php
@@ -14,6 +14,7 @@
 class CalculaValores {
 
     static $nome;
+    static $variavelPEGAPAPG;
 
     public function CalculaPA($razao, $numseq, $a1) {
         $vetor = array();
@@ -60,93 +61,260 @@ class CalculaValores {
     }
 
     public function verificaehString($vetor) {
+        //  if() // se é numero... 
+        //se é string letras ne?
+
+
         $count = count($vetor);
         for ($i = 0; $i < $count; $i++) {
-            if (is_string($vetor[$i])) {
-                return ($i);
+
+            if (count($vetor) == 0) {
+                return TRUE;
+            } else if (!(is_numeric($vetor[$i]))) {
+                if (is_string($vetor[$i])) {
+                    return FALSE;
+                } else {
+                    return TRUE;
+                }
+            } else if (is_string($vetor[$i])) {
+                return FALSE;
             }
         }
+
+        return FALSE;
     }
 
     public function calculaSoma($vet2) {
+        $soma = 0;
         $qtd = count($vet2);
         for ($i = 0; $i < $qtd; $i++) {
-            $soma = $soma + $vet2[$i];
+            if (is_numeric($vet2[$i])) {
+                $soma = $soma + $vet2[$i];
+            }
         }
         return $soma;
     }
 
     public function calculaMedia($vet2) {
         $qtd = count($vet2);
-        $media = self::calculaSoma($vet2) / $qtd;
-        return $media;
+
+        $resultadosoma = self::calculaSoma($vet2) / $qtd;
+        return $resultadosoma;
     }
 
     public function calculaMediana($vet2) {
         $qtd = count($vet2);
+
         if ($qtd % 2 == 0) {
-            $nummedia = $qtnd / 2; // 10 vai ficar 5
-            $mediana = ($vet2[$nummedia] + $vet2[$nummedia + 1]) / 2;
+            $nummedia = $qtd / 2;
+            if (is_numeric($vet2[$numedia]) && (is_numeric($vet2[$numedia + 1]))) {
+                return $mediana = ($vet2[$nummedia] + $vet2[$nummedia + 1]) / 2;
+            } else {
+                return 0;
+            }
         } else {
-            $nummedia = $qtnd / 2;
-            $mediana = floor(($vet2[$nummedia]));
+            $nummedia = $qtd / 2;
+            if (is_numeric($vet2[$nummedia])) {
+                return $mediana = floor(($vet2[$nummedia]));
+            } else {
+                return 0;
+            }
         }
     }
 
+  
     public function returnVetorGrafico($vet2) {
+
         $qtd = count($vet2);
-        $array = ['Sequencia', 'teste'];
-        $qtdarray = count($array);
-        $grafico[0] = $array; // grafico[0][0]e grafico[0][1]
-        for ($i = 1; $i < ($qtd + 1); $i++) {
+
+        $vetor;
+
+        for ($i = 0; $i < $qtd; $i++) {
+            if (!is_numeric($i)) {
+                return $vetor;
+            }
+        }
+        for ($i = 0; $i < $qtd; $i++) {
             $newarray = [];
-            $newarray = [$i, $vet2[($i - 1)]];
+            $newarray = [$i+1, $vet2[($i)]];
             $grafico[$i] = $newarray;
         }
-       
-        $contagem = count($grafico);
 
-       // for ($o = 0; $o < $contagem; $o++) {
-      //      for ($p = 0; $p < 2; $p++) {
-     //           echo $grafico[$o][$p] . " ";
-     //       }
-    //    }
+        $nome = "teste123";
+
+        self::ObjParaJson($grafico, $nome);
+
+        /* $contagem = count($grafico);
+
+          // for ($o = 0; $o < $contagem; $o++) {
+          //      for ($p = 0; $p < 2; $p++) {
+          //           echo $grafico[$o][$p] . " ";
+          //       }
+          //    }
 
 
-        $stringvolta = "";
+          $stringvolta = "";
 
-        for ($o = 0; $o < $contagem; $o++) {
-            $stringvolta = $stringvolta . '[';
-            for ($p = 0; $p < 2; $p++) {
-                if (($o === 0) && ($p == 0)) {
-                    $stringvolta = $stringvolta . "'";
-                } else
-                if (($o === 0) && ($p == 1)) {
-                    $stringvolta = $stringvolta . "'";
-                }
-                $stringvolta = $stringvolta . $grafico[$o][$p];
-                if (($o === 0) && ($p == 0)) {
-                    $stringvolta = $stringvolta . "',";
-                } else
-                if (($o === 0) && ($p == 1)) {
-                    //$stringvolta = $stringvolta . "',";
-                } else if ($p != 1) {
-                    $stringvolta = $stringvolta . ",";
-                }
-            }
-            
-            if (($o != ($contagem-1)) && ($p != 1)) {
-                $stringvolta = $stringvolta . "],";
-            }else{
-                $stringvolta = $stringvolta . "]";
-            }
-        }
+          for ($o = 0; $o < $contagem; $o++) {
+          $stringvolta = $stringvolta . '[';
+          for ($p = 0; $p < 2; $p++) {
+          if (($o === 0) && ($p == 0)) {
+          $stringvolta = $stringvolta . "'";
+          } else
+          if (($o === 0) && ($p == 1)) {
+          $stringvolta = $stringvolta . "'";
+          }
+          $stringvolta = $stringvolta . $grafico[$o][$p];
+          if (($o === 0) && ($p == 0)) {
+          $stringvolta = $stringvolta . "',";
+          } else
+          if (($o === 0) && ($p == 1)) {
+          //$stringvolta = $stringvolta . "',";
+          } else if ($p != 1) {
+          $stringvolta = $stringvolta . ",";
+          }
+          }
+
+          if (($o != ($contagem - 1)) && ($p != 1)) {
+          $stringvolta = $stringvolta . "],";
+          } else {
+          $stringvolta = $stringvolta . "]";
+          }
+          } */
 
         return $grafico;
     }
 
+    public function calculaPorcentagem($vet2, $verdadeiroPA, $verdadeiroPG) {
+        $cont = count($vet2);
+        if ($vet2[2] == "PA") {
+            return 100;
+        } else if ($vet2[2] == "PG") {
+            return 100;
+        } else {
+
+            $contpa = 0;
+            $contpg = 0;
+
+            for ($i = 0; $i < $cont; $i++) {
+                if ($verdadeiroPA[$i] == $vet2[$i]) {
+                    $contpa = $contpa + 1;
+                }
+                if ($verdadeiroPG[$i] == $vet2[$i]) {
+                    $contpg = $contpg + 1;
+                }
+            }
+
+            if ($contpa > $contpg) {
+                self::$variavelPEGAPAPG = "PA";
+                $conta = 100 * $contpa;
+                return $porcentagem = $conta / $cont;
+            } else if ($contpg > $contpa) {
+                self::$variavelPEGAPAPG = "PG";
+                $conta = 100 * $contpg;
+                return $porcentagem = $conta / $cont;
+            } else {
+                self::$variavelPEGAPAPG = "PAPG";
+                $porcentagem = 50;
+                return $porcentagem;
+            }
+        }
+    }
+
     public function verificaPAPG($vet2) {
-        
+
+        $vetor;
+
+
+        if (self::verificaehString($vet2) == FALSE) {
+
+            $cont;
+            $primeiroelemento = $vet2[0];
+            $segundoelemento = $vet2[1];
+            $numSeq = count($vet2);
+            //3             //1   = razao   = 2
+            $testeRazaoPA = $segundoelemento - $primeiroelemento;
+            //3            /   1    =   3 
+            $testeRazaoPG = $segundoelemento / $primeiroelemento;
+            $verdadeiroPA = self::CalculaPA($testeRazaoPA, $numSeq, $primeiroelemento);
+            $verdadeiroPG = self::CalculaPG($testeRazaoPG, $numSeq, $primeiroelemento);
+
+            //$porcentagem
+
+            $terceiroelementoPA = $verdadeiroPA[2]; //
+            $terceiroelementoPG = $verdadeiroPG[2]; //
+
+            $vetor[0] = $primeiroelemento; // elemento A1
+            // lembrar de colocar if para averiguar se há algum objeto como letra, ou caracteres especiais
+            if ($terceiroelementoPA == $vet2[2]) {
+
+                for ($i = 3; $i < $numSeq; $i++) {
+                    if ($verdadeiroPA[$i] != $vet2[$i]) {
+                        $vetor[1] = $testeRazaoPA;
+                        $vetor[2] = "N";
+                        $vetor[3] = $numSeq;
+                        $vetor[4] = self::calculaSoma($vet2);
+                        $vetor[5] = self::calculaMedia($vet2);
+                        $vetor[6] = self::calculaMediana($vet2);
+                        $vetor[7] = self::calculaPorcentagem($vet2, $verdadeiroPA, $verdadeiroPG);
+                        $vetor[8] = self::$variavelPEGAPAPG;
+                        return $vetor;
+                    }
+                }
+                $vetor[1] = $testeRazaoPA;
+                $vetor[2] = "PA";
+                $vetor[3] = $numSeq;
+                $vetor[4] = self::calculaSoma($vet2);
+                $vetor[5] = self::calculaMedia($vet2);
+                $vetor[6] = self::calculaMediana($vet2);
+                $vetor[7] = self::calculaPorcentagem($vet2, $verdadeiroPA, $verdadeiroPG);
+                return $vetor;
+            } else if ($terceiroelementoPG == $vet2[2]) {
+                for ($i = 3; $i < $numSeq; $i++) {
+                    if ($verdadeiroPG[$i] != $vet2[$i]) {
+                        $vetor[1] = 0;
+                        $vetor[2] = "N";
+                        $vetor[3] = $numSeq;
+                        $vetor[4] = self::calculaSoma($vet2);
+                        $vetor[5] = self::calculaMedia($vet2);
+                        $vetor[6] = self::calculaMediana($vet2);
+                        $vetor[7] = self::calculaPorcentagem($vet2, $verdadeiroPA, $verdadeiroPG);
+                        $vetor[8] = self::$variavelPEGAPAPG;
+                        return $vetor;
+                    }
+                }
+                $vetor[1] = $testeRazaoPG;
+                $vetor[2] = "PG";
+                $vetor[3] = $numSeq;
+                $vetor[4] = self::calculaSoma($vet2);
+                $vetor[5] = self::calculaMedia($vet2);
+                $vetor[6] = self::calculaMediana($vet2);
+                $vetor[7] = self::calculaPorcentagem($vet2, $verdadeiroPA, $verdadeiroPG);
+                return $vetor;
+            } else {
+                $vetor[1] = 0;
+                $vetor[2] = "N";
+                $vetor[3] = $numSeq;
+                $vetor[4] = self::calculaSoma($vet2);
+                $vetor[5] = self::calculaMedia($vet2);
+                $vetor[6] = self::calculaMediana($vet2);
+                $vetor[7] = self::calculaPorcentagem($vet2, $verdadeiroPA, $verdadeiroPG);
+                $vetor[8] = self::$variavelPEGAPAPG;
+
+                return $vetor;
+            }
+        } else {
+            $vetor[0] = 0;
+            $vetor[1] = 0;
+            $vetor[2] = "N";
+            $vetor[3] = 0;
+            $vetor[4] = 0;
+            $vetor[5] = 0;
+            $vetor[6] = 0;
+            $vetor[7] = 0;
+
+            return $vetor;
+        }
     }
 
     public static function setarNomeArq($nomet) {
@@ -155,6 +323,30 @@ class CalculaValores {
 
     public static function getNomeArq() {
         return Self::$nome;
+    }
+
+    public function verificaJson($dados) {
+        if (self::verificaehString($dados)) {
+
+            $contador = count($dados);
+
+            // se for diferente de so numero
+            $str = "";
+
+            for ($i = 0; $i < $contador; $i++) {
+                $str = $str . $dados[$i];
+            }
+
+            $tam = strlen($str);
+
+            for ($j = 0; $j < $tam; $j++) {
+                if (!str_contains($str, '"')) {
+                    return FALSE;
+                }
+            }
+        } else {
+            return TRUE;
+        }
     }
 
 }
--- JanelaMostraDados.php
+++ JanelaMostraDados.php
@@ -10,10 +10,44 @@ session_start();
 require_once (realpath($_SERVER["DOCUMENT_ROOT"]) . "\SistemaPAPG\SistemaPAPG\ArquivosPHP\CalculaValores.php");
 $nomearq = $_SESSION['novonome'];
 $classe = new CalculaValores();
+
 $dados = $classe->JsonParaObj($nomearq);
 
-//echo $classe->returnVetorGrafico($dados);
-$stringteste = $classe->returnVetorGrafico($dados);
+$ndados;
+$z1 = 0;
+$z2 = 0;
+
+
+if (empty($dados)) {
+    $pera = FALSE;
+    $variavelvetordados[0] = 0;
+    $variavelvetordados[1] = 0;
+    $variavelvetordados[2] = "";
+    $variavelvetordados[3] = 0;
+    $variavelvetordados[4] = 0;
+    $variavelvetordados[5] = 0;
+    $variavelvetordados[6] = 0;
+    $variavelvetordados[7] = 0;
+} else {
+    $pera = $classe->verificaJson($dados);
+
+    if ($pera == TRUE) {
+        $ndados = count($dados);
+        $stringteste = $classe->returnVetorGrafico($dados); // gera um vetor de um vetor[0] [0] [1]
+
+        $variavelvetordados = $classe->verificaPAPG($dados);
+    } else {
+
+        $variavelvetordados[0] = 0;
+        $variavelvetordados[1] = 0;
+        $variavelvetordados[2] = "";
+        $variavelvetordados[3] = 0;
+        $variavelvetordados[4] = 0;
+        $variavelvetordados[5] = 0;
+        $variavelvetordados[6] = 0;
+        $variavelvetordados[7] = 0;
+    }
+}
 ?>
 
 <html>
@@ -27,23 +61,94 @@ $stringteste = $classe->returnVetorGrafico($dados);
         <div> 
             <h1 name="titulotopo" id="titulotopo"> SISTEMA PAPG</h1>
         </div>
-         <div id="curve_chart" style="width: 900px; height: 500px"></div>
+        <div id="curve_chart" style="width: 900px; height: 500px"></div>
+
+        <div style="color:red;">
+            <?php
+            if ($pera == FALSE) {
+                echo "<h1> ERRO! Arquivo Com Erro, Insira um Arquivo Completo e Sem Erros!</h1>";
+            }
+            ?>
+        </div>
+
+
+
+        <div id="DadosTeste" style="width: 900px; height: 500px">
+            <br><br>
+            <label>Elemento A1 (Primeiro Termo):</label>
+            <input id="inputa1" type="text" name="inputa1" value=<?php echo $variavelvetordados[0] ?> readonly>
+            <br><br>
+
+            <label>Valor de R (Razão):</label>
+            <input id="inputr" type="text" name="inputr" value=<?php echo $variavelvetordados[1] ?> readonly>
+            <br><br>
+
+            <label>Quantidade De Termos (Elementos Da Sequencia):</label>
+            <input id="inputqtde" type="text" name="inputqtde" value=<?php echo $variavelvetordados[3] ?> >
+            <br><br>
+
+            <label>Selecione o Tipo de Progressão ao Lado:</label>
+            <INPUT TYPE="RADIO" NAME="opcaoPAPG" id="inputpapg" VALUE="op1" checked> <?php
+            if ($variavelvetordados[2] == "N") {
+                echo "Nem PA, Nem PG";
+            } else if ($variavelvetordados[2] == "PA") {
+                echo "PA";
+            } else {
+                echo "PG";
+            }
+            ?>
+            <br><br>
+            <label>Soma Dos Termos:</label>
+            <input id="inputb1" type="text" name="inputb1" value=<?php echo $variavelvetordados[4] ?> readonly>
+
+            <br><br>
+            <label>Média Dos Termos:</label>
+            <input id="inputb2" type="text" name="inputb2" value=<?php echo $variavelvetordados[5] ?> readonly>
+
+            <br><br>
+            <label>Mediana Dos Termos:</label>
+            <input id="inputb2" type="text" name="inputb2" value=<?php echo $variavelvetordados[6] ?> readonly>
+            <br><br>
+
+            <label>Porcentagem:</label>
+            <input id="inputb4" type="text" name="inputb4" value=<?php
+                   echo $variavelvetordados[7] . "% ";
+                   ?> readonly> <?php
+                   if ($variavelvetordados[2] == "N") {
+                       echo $variavelvetordados[8];
+                   }
+                   ?>
+            <br><br>
+
+        </div>
 
 
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
             google.charts.load('current', {'packages': ['corechart']});
             google.charts.setOnLoadCallback(drawChart);
-            teste = <?php echo $stringteste; ?>
+            valortotal =<?php echo $ndados; ?>;
+
+            var matriz = new Array(valortotal);
+              for (var i = 0; i < valortotal; i++) {
+                
+                matriz[i] = [<?php echo $stringteste[$z1][$z2]; ?>,<?php echo $stringteste[$z1][$z2 + 1]; ?>];
+               
+                   document.write("pos:"+i)
+                   
+                   document.write(matriz[i]);
+                   
+            }
+
             
+
+
+
             function drawChart() {
-                var data = google.visualization.arrayToDataTable([
-                    ['Year', 'Sales', 'Expenses'],
-                    ['2004', 1000, 400],
-                    ['2005', 1170, 460],
-                    ['2006', 660, 1120],
-                    ['2007', 1030, 540]
-                ]);
+                var data = new google.visualization.DataTable();
+                data.addColumn('number', 'Sequencia');
+                data.addColumn('number', 'PAPG');
+
 
 
                 var options = {
@@ -51,12 +156,10 @@ $stringteste = $classe->returnVetorGrafico($dados);
                     curveType: 'function',
                     legend: {position: 'bottom'}
                 };
-
-
                 var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
                 chart.draw(data, options);
             }
         </script>
+
     </body>
 </html>
